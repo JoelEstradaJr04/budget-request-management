f33fe23d7ea35c23d1bb9c562d60e0687de77218
Brian S. Caube <caube.brian@gmail.com>
2026-01-08 23:32:03 +0800

    remove export

--- frontend/app/(pages)/budget-management/budgetRequest/page.tsx
+++ frontend/app/(pages)/budget-management/budgetRequest/page.tsx
@@ -753,24 +753,6 @@ const BudgetRequestPage = () => {
           </div>
 
           <div className="filters">
-            {/* Export dropdown */}
-            <div className="export-dropdown">
-              <button className="export-dropdown-toggle">
-                <i className="ri-download-line" /> Export
-              </button>
-              <div className="export-dropdown-menu">
-                <button onClick={() => handleExport('csv')}>
-                  <i className="ri-file-text-line" /> CSV
-                </button>
-                <button onClick={() => handleExport('excel')}>
-                  <i className="ri-file-excel-line" /> Excel
-                </button>
-                <button onClick={() => handleExport('pdf')}>
-                  <i className="ri-file-pdf-line" /> PDF
-                </button>
-              </div>
-            </div>
-
             {/* Add New Request */}
             <button onClick={() => openModal('add')} id="addButton" className="addButton">
                 <i className="ri-add-line" />New Request
